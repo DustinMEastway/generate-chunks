5b7aa52c0a054c4584e1ee52c4a867f069dc5712
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2023-01-23 18:33:47 -0500

    update block size in scene instead of using the script

--- World/Blocks/Block.cs
+++ World/Blocks/Block.cs
@@ -6,6 +6,7 @@ public class Block : StaticBody2D {
 	public static readonly long Width = 6;
 	public static readonly Vector2 Size = new Vector2(Block.Width, Block.Height);
 	public static readonly Color DefaultColor = new Color(1, 1, 1);
+	private static bool _TestBlockSize = true;
 
 	/// <summary>Color to use for the block</summary>
 	public Color Color;
@@ -17,11 +18,21 @@ public class Block : StaticBody2D {
 	}
 
 	public override void _Ready() {
+		base._Ready();
 		var collisionShape = GetNode<CollisionShape2D>("CollisionShape");
 		var colorRect = GetNode<ColorRect>("ColorRect");
 		colorRect.Color = Color;
-		colorRect.RectSize = Block.Size;
-		(collisionShape.Shape as RectangleShape2D).Extents = Block.Size;
-		collisionShape.Position = Block.Size / 2;
+
+		if (!Block._TestBlockSize) {
+			return;
+		}
+		Block._TestBlockSize = false;
+		if (
+			colorRect.RectSize != Block.Size
+			|| (collisionShape.Shape as RectangleShape2D).Extents != Block.Size
+			|| collisionShape.Position != Block.Size / 2
+		) {
+			throw new Exception($"{nameof(Block)}.{nameof(Block.Size)} must match node sizes.");
+		}
 	}
 }
