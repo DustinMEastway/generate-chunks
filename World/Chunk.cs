f5b757ec35cb8f12b2cc03f2192fda0898e3c477
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2023-01-23 20:49:56 -0500

    refactor noise refactor (#3)
    * refactor chunk
    
    * fix block resizing issues
    
    * update block size in scene instead of using the script

--- World/Chunk.cs
+++ World/Chunk.cs
@@ -24,67 +24,22 @@ public class Chunk : Node2D {
 		Noise.Persistence = 0.8f;
 	}
 
-	private int[][] _GenerateArray(long width, long height, bool empty) {
-		int[][] map = new int[width][];
-		for (int columnI = 0; columnI < width; columnI++) {
-			map[columnI] = new int[height];
-			for (int blockI = 0; blockI < height; blockI++) {
-				map[columnI][blockI] = empty ? 0 : 1;
-			}
-		}
-
-		return map;
-	}
-
-	private int[][] _TerrainGeneration(int[][] map, long width, long height) {
-		int noiseHeight;
-		for (int columnI = 0; columnI < width; columnI++) {
-			noiseHeight = Mathf.RoundToInt(Noise.GetNoise2d((columnI / Smoothness), Seed) * (height * 2));
-			// noiseHeight += Mathf.RoundToInt(height / 2);
-			for (int blockI = 0; (blockI < height && blockI < noiseHeight); blockI++) {
-				map[columnI][blockI] = 1;
-			}
-		}
-
-		return map;
-	}
-
 	private void _RenderBlocks() {
-		long width = Chunk.ChunkBlockWidth;
-		long height = Chunk.DefaultGroundLevel / 2;
-		int[][] map;
+		for (var columnI = 0; columnI < Chunk.ChunkBlockWidth; columnI++) {
+			var groundLevel = Chunk.DefaultGroundLevel + Mathf.RoundToInt(
+				Noise.GetNoise2d((columnI / Smoothness), Seed) * Chunk.DefaultGroundLevel
+			);
 
-		// generate underground layer
-		int[][] undergroundMap = _GenerateArray(width, height, false);
-
-		// generate ground layer
-		int[][] groundMap = _GenerateArray(width, height, true);
-		groundMap = _TerrainGeneration(groundMap, width, height);
-
-		// stack layers
-		map = new int[undergroundMap.Length][];
-		undergroundMap.CopyTo(map, 0);
-		for (int columnI = 0; columnI < groundMap.Length; columnI++) {
-			int[] newColumn = new int[map[columnI].Length + groundMap[columnI].Length];
-			map[columnI].CopyTo(newColumn, 0);
-			groundMap[columnI].CopyTo(newColumn, undergroundMap[columnI].Length);
-			map[columnI] = newColumn;
-		}
-
-		for (int columnI = 0; columnI < width; columnI++) {
-			for (int blockI = 0; blockI < map[columnI].Length; blockI++) {
+			for (var blockI = 0; blockI < groundLevel; blockI++) {
 				Block block = null;
-				float noise = Noise.GetNoise2d(columnI, blockI);
-				int lastIndex = Array.FindLastIndex(map[columnI], i => i == 1);
+				var noise = Noise.GetNoise2d(columnI, blockI);
 
-				if (blockI < height || map[columnI][blockI] == 1) {
-					if (lastIndex == blockI) {
-						block = new Grass();
-					} else if (noise < -0.1) {
-						block = new Stone();
-					} else {
-						block = new Dirt();
-					}
+				if (blockI == groundLevel - 1) {
+					block = new Grass();
+				} else if (noise < -0.1) {
+					block = new Stone();
+				} else {
+					block = new Dirt();
 				}
 
 				if (block != null) {
