71ee7a41936f75f618fd3b9eccbdeeb16569df8e
Dustin M. Eastway <Dustin.Eastway@gmail.com>
2023-01-31 07:34:26 -0500

    spawn chunks as the player moves and loop the world

--- World/Chunk.cs
+++ World/Chunk.cs
@@ -23,26 +23,47 @@ public class BlockId {
 	}
 }
 
-public class Chunk : Node2D {
+public class Chunk : Area2D {
 	public static readonly int BlockHeight = 6;
 	public static readonly int BlockWidth = 6;
+	public static readonly Vector2 BlockSize = new Vector2(
+		Chunk.BlockWidth,
+		Chunk.BlockHeight
+	);
 	public static readonly int ChunkBlockHeight = 64;
 	public static readonly int ChunkBlockWidth = 64;
+	public static readonly Vector2 ChunkBlockSize = new Vector2(
+		Chunk.ChunkBlockWidth,
+		Chunk.ChunkBlockHeight
+	);
 	public static readonly int DefaultGroundLevel = 32;
-	public bool IsReady { get; private set; } = false;
 	public int ChunkId;
+	public bool IsReady { get; private set; } = false;
+	public int ChunkOffset;
+	[Signal]
+	public delegate void PlayerEntered(Player player, Chunk chunk);
 	public float Smoothness = 4;
 	public TileMap TileMap;
 	private BlockId _BlockId;
 	private int _BlockOffset;
 
 	public override void _Ready() {
+		base._Ready();
 		TileMap = GetNode<TileMap>("TileMap");
+		var collisionShape = GetNode<CollisionShape2D>("CollisionShape");
+		var halfChunkSize = Chunk.ChunkBlockSize * Chunk.BlockSize / 2;
+		collisionShape.Position = halfChunkSize;
+		(collisionShape.Shape as RectangleShape2D).Extents = halfChunkSize;
 		_BlockId = new BlockId(TileMap);
 		_BlockOffset = ChunkId * Chunk.ChunkBlockWidth;
-		Position = new Vector2(_BlockOffset * Chunk.BlockWidth, 0);
 		IsReady = true;
 		RenderBlocks();
+	}
+
+	public void _OnChunkBodyEntered(Node node) {
+		if (node is Player) {
+			EmitSignal(nameof(PlayerEntered), node, this);
+		}
 	}
 
 	public void RenderBlocks() {
--- World/World.cs
+++ World/World.cs
@@ -1,13 +1,15 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class World : Node2D {
 	public static readonly PackedScene ChunkScene = ResourceLoader.Load<PackedScene>("res://World/Chunk.tscn");
 	public static readonly OpenSimplexNoise Noise = new OpenSimplexNoise();
 	public static readonly PackedScene PlayerScene = ResourceLoader.Load<PackedScene>("res://Player/Player.tscn");
-	public static readonly Random _Random = new Random();
-	public static int Seed { get; private set; } = _Random.Next();
+	public static readonly Random Random = new Random();
+	public static int Seed { get; private set; } = Random.Next();
+	public static int WorldChunkWidth = 3;
 	private Camera2D _Camera;
 	private Player _Player = null;
 	private Dictionary<int, Chunk> _RenderedChunks = new Dictionary<int, Chunk>();
@@ -17,7 +19,9 @@ public class World : Node2D {
 	);
 
 	public override void _Ready() {
-		_ChangeSeed();
+		World.Noise.Octaves = 4;
+		World.Noise.Period = 20.0f;
+		World.Noise.Persistence = 0.8f;
 		_Camera = GetNode<Camera2D>("Camera");
 		_SpawnChunk(-1);
 		_SpawnChunk(0);
@@ -25,10 +29,36 @@ public class World : Node2D {
 		_SpawnPlayer();
 	}
 
-	private void _SpawnChunk(int i) {
+	private void _OnPlayerEnteredChunk(Player player, Chunk chunk) {
+		// Render chunks around the player
+		var maxChunk = chunk.ChunkOffset + 2;
+		var minChunk = chunk.ChunkOffset - 2;
+		for (int i = minChunk; i <= maxChunk; ++i) {
+			CallDeferred(nameof(_SpawnChunk), i);
+		}
+
+		// Remove chunks far from the player
+		var farAwayChunkOffsets = _RenderedChunks.Keys.Where((offset) => {
+			return offset > maxChunk || offset < minChunk;
+		}).ToArray();
+		foreach (var chunkOffset in farAwayChunkOffsets) {
+			RemoveChild(_RenderedChunks[chunkOffset]);
+			_RenderedChunks.Remove(chunkOffset);
+		}
+	}
+
+	private void _SpawnChunk(int chunkOffset) {
+		if (_RenderedChunks.ContainsKey(chunkOffset)) {
+			return;
+		}
+
 		var chunk = World.ChunkScene.Instance<Chunk>();
-		chunk.ChunkId = i;
-		_RenderedChunks.Add(i, chunk);
+		var id = chunkOffset % World.WorldChunkWidth;
+		chunk.ChunkId = (id < 0) ? id + World.WorldChunkWidth : id;
+		chunk.ChunkOffset = chunkOffset;
+		chunk.Position = new Vector2(chunkOffset * Chunk.ChunkBlockWidth * Chunk.BlockWidth, 0);
+		chunk.Connect(nameof(Chunk.PlayerEntered), this, nameof(_OnPlayerEnteredChunk));
+		_RenderedChunks.Add(chunkOffset, chunk);
 		AddChild(chunk);
 	}
 
@@ -52,11 +82,8 @@ public class World : Node2D {
 	}
 
 	private void _ChangeSeed() {
-		World.Seed = _Random.Next();
+		World.Seed = Random.Next();
 		World.Noise.Seed = Seed;
-		World.Noise.Octaves = 4;
-		World.Noise.Period = 20.0f;
-		World.Noise.Persistence = 0.8f;
 		foreach (var renderedChunk in _RenderedChunks.Values) {
 			renderedChunk.RenderBlocks();
 		}
